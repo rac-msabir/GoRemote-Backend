4c8e982c02b7760904ff9faf5d24ae23c815152c
Bilal Hassan <bilalhsn226@gmail.com>
2025-10-09 19:40:52 +0500

    add vercel url for cors error

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://go-remote-frontend-w27j.vercel.app', // ← no trailing slash
+],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
